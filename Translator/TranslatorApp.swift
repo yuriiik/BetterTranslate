43e8fd8b6da8c53a4faa3bd71c05683fa8317c51
Yurii Kupratsevych <kupratsevich@gmail.com>
2025-08-18 11:53:41 +0300

    Initial Commit

---
+++ Translator/TranslatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TranslatorApp.swift
+//  Translator
+//
+//  Created by Yurii Kupratsevych on 18.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct TranslatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
