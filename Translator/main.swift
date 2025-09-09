a82d24c4ca022386e0c9597b6e0fedfa8d8739d9
Yurii Kupratsevych <kupratsevich@gmail.com>
2025-09-09 11:46:44 +0300

    switch to a headless AppKit app

---
+++ Translator/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  Translator
+//
+//  Created by Yurii Kupratsevych on 09.09.2025.
+//
+
+import AppKit
+
+let app = NSApplication.shared
+let delegate = AppDelegate()
+app.delegate = delegate
+app.run()
